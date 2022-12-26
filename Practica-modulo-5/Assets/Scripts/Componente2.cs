1a638ad780192051b82735c1c98a3b66bfded0a9
loecelia <118191091+loecelia@users.noreply.github.com>
2022-12-25 18:00:20 -0600

    Practica 5
    Hola mundo y funciones game loop

---
+++ Practica-modulo-5/Assets/Scripts/Componente2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Componente2 : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Componente1.miObjeto.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Practica-modulo-5/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola mundo");
+        Debug.LogWarning("El aire se contamina");
+        Debug.LogError("El agua se acaba");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
