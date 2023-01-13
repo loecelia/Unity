2b73d6b4bcd7fa5677fea9f1615679615ab7eee3
loecelia <118191091+loecelia@users.noreply.github.com>
2023-01-12 21:17:50 -0600

    BrakeOut
    Práctica Modulo 12

--- BreakOut/Assets/Scenes/_Scripts/Jugador.cs
+++ BreakOut/Assets/Scenes/_Scripts/Jugador.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float VelocidadPaddle = 30f;
+
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,36 @@ public class Jugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //mousePos2D = Input.mousePosition;
+        //mousePos2D.z = -Camera.main.transform.position.z;
+        //mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    this.transform.Translate(Vector3.down * VelocidadPaddle * Time.deltaTime);
+
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    this.transform.Translate(Vector3.up * VelocidadPaddle * Time.deltaTime);
+        //}
+
+        this.transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * VelocidadPaddle * Time.deltaTime);
+
+        Vector3 pos = this.transform.position;
+        //pos.x = mousePos3D.x;
+        
+        //limitar limites del mouse 
+        if(pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        } else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos;
         
     }
 }
